cb060c1e3602273b54eefc05ca200ba51b6ec1a2
Ethan Chen <ethan.chen@oculus.com>
2020-08-14 22:32:45 -0700

    Initial script checkin

---
+++ hud_packet.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import socket
+
+
+def send_msg():
+    hud = ("192.168.10.1", 50007)
+
+    hello_msg = [
+        0x7a, 0x01, 0x01, 0x15,
+        0x13, 0x01
+    ]
+
+    server_ack_msg = [
+        0x7c, 0x04, 0x01, 0x00,
+        0x00
+    ]
+
+    msg = [
+        0x7a, 0x02, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00,
+        0x00, 0xff, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x01,
+        0x00, 0x01
+    ]
+    raw_msg = bytearray(msg)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(hud)
+    s.sendall(raw_msg)
+    data = s.recv(1024)
+
+
+def main():
+    send_msg()
+
+
+if __name__ == "__main__":
+    main()
